41b0365e9c0d7b71aafc76dfc6df3b0216682836
BiliksuunSamuel <biliksuunsamuel@gmail.com>
2022-11-16 16:01:58 +0000

    state value error

--- features/auth/authReducer.ts
+++ features/auth/authReducer.ts
@@ -4,18 +4,9 @@ import { AuthReducerState } from "../../store/state";
 const authReducer = createSlice({
   name: "authReducer",
   initialState: AuthReducerState,
-  reducers: {
-    resetAuth: (state) => {
-      state.user = null;
-      state.loading = false;
-      state.error = null;
-    },
-    authPending: (state) => {
-      state.loading = true;
-    },
-  },
+  reducers: {},
   extraReducers: (builder) => {},
 });
 
 export default authReducer.reducer;
-export const { resetAuth, authPending } = authReducer.actions;
+export const {} = authReducer.actions;
