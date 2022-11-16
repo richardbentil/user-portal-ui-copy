9df7ea160736314351c06ca4761b862c445c75a7
BiliksuunSamuel <biliksuunsamuel@gmail.com>
2022-11-16 10:40:49 +0000

    api configuration

--- routes/apiRoutes.ts
+++ routes/apiRoutes.ts
@@ -1,10 +1,12 @@
 export const APIRoutes = {
   ghanaGov: {
     root: "https://api.ghana.gov.gh/",
+    root1: "https://www.govgh.org/",
     createInvoice: "api/v1.0/checkout/invoice.php",
     getInvoiceDetails: "api/v1.0/checkout/invoice.php",
     searchMDABranch: "api/v1.0/checkout/mda_branch.php",
     searchServices: "api/v1.0/checkout/service.php",
     oauth: "oauth",
+    getToken: "api/v1.0/oauth/token.php",
   },
 };
