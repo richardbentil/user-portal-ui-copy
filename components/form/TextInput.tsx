1939e7ad2a79030bb484337e1bd4fa6745c8f646
BiliksuunSamuel <biliksuunsamuel@gmail.com>
2022-11-21 14:02:43 +0000

    clean up for test

--- components/form/TextInput.tsx
+++ components/form/TextInput.tsx
@@ -1,17 +1,22 @@
-import { useField } from 'formik'
-import React from 'react'
+import { useField } from "formik";
+import React from "react";
 
-function TextInput({ ...props }) {
-  const [field, meta] = useField(props)
-  const { touched, error } = meta
-  const err = error && touched
+function TextInput({ ...props }: any) {
+  const [field, meta] = useField(props);
+  const { touched, error } = meta;
+  const err = error && touched;
 
   return (
     <>
-      <input {...field} {...props}  className={`form-control ${err ? "is-invalid" : ""}`} required />
-      {err && <small className='form-text text-danger'>{error}</small>}
+      <input
+        {...field}
+        {...props}
+        className={`form-control ${err ? "is-invalid" : ""}`}
+        required
+      />
+      {err && <small className="form-text text-danger">{error}</small>}
     </>
-  )
+  );
 }
 
-export default TextInput
+export default TextInput;
